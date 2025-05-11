68e16bb29735dad6f85b0a4c80c8df51315e39ec
Your Name <you@example.com>
2025-05-11 13:18:28 +0200

    update: burp extension

--- extensions/BurpExtension/h2cSmugglingCheck.py
+++ extensions/BurpExtension/h2cSmugglingCheck.py
@@ -1,115 +1,110 @@
+# -*- coding: utf-8 -*-
 from burp import IBurpExtender
-from burp import IScannerCheck
 from burp import IScanIssue
+from burp import IContextMenuFactory
+from javax.swing import JMenuItem
+from java.util import ArrayList
+from java.awt.event import ActionListener
+import threading
 
 
-class BurpExtender(IBurpExtender, IScannerCheck):
+class BurpExtender(IBurpExtender, IContextMenuFactory):
 
     def registerExtenderCallbacks(self, callbacks):
         self._callbacks = callbacks
         self._helpers = callbacks.getHelpers()
         callbacks.setExtensionName("h2cSmuggler")
-        callbacks.registerScannerCheck(self)
-        self.urlLastScanned = None
+        callbacks.registerContextMenuFactory(self)
 
-    def doPassiveScan(self, baseRequestResponse):
-        return None
+    def createMenuItems(self, contextMenuInvocation):
+        menuItems = ArrayList()
+        menuItem = JMenuItem("Scan for h2c Smuggling")
+        menuItem.addActionListener(H2cSmugglingActionListener(self, contextMenuInvocation))
+        menuItems.add(menuItem)
+        return menuItems
 
-    def doActiveScan(self, baseRequestResponse, insertionPoint):
-        request = baseRequestResponse.getRequest()
-        requestInfo = self._helpers.analyzeRequest(baseRequestResponse)
-        body = request[requestInfo.getBodyOffset():]
 
-        # Avoid scanning the same endpoint for multiple insertion points
-        if self.urlLastScanned == requestInfo.getUrl():
-            return None
-        self.urlLastScanned = requestInfo.getUrl()
+class H2cSmugglingActionListener(ActionListener):
+    def __init__(self, extender, invocation):
+        self._extender = extender
+        self._invocation = invocation
 
-        # More likely a false positive for cleartext connections
-        confidence = "Certain"
-        if baseRequestResponse.getHttpService().getProtocol != "https":
-            confidence = "Tentative"
+    def actionPerformed(self, event):
+        # Lancer le scan dans un thread séparé
+        threading.Thread(target=self.run_scan).start()
 
-        # Replace headers in original request
-        headers = requestInfo.getHeaders()
-        newHeaders = []
-        for header in headers:
-            if header.startswith("Connection") or header.startswith("Upgrade"):
-                pass
-            else:
-                newHeaders.append(header)
-        newHeaders.append("Upgrade: h2c")
-        newHeaders.append("HTTP2-Settings: AAMAAABkAARAAAAAAAIAAAAA")
+    def run_scan(self):
+        selectedMessages = self._invocation.getSelectedMessages()
+        self._extender._callbacks.printOutput("Selected messages: " + str(len(selectedMessages)))
+        if not selectedMessages:
+            self._extender._callbacks.printOutput("No messages selected")
+            return
 
-        # Build Requests
-        connStr = "Connection: Upgrade, HTTP2-Settings"
-        h2cRequestOne = self._helpers.buildHttpMessage(newHeaders + [connStr],
-                                                       body)
+        for baseRequestResponse in selectedMessages:
+            try:
+                self._extender._callbacks.printOutput("Processing request to: " + str(baseRequestResponse.getUrl()))
+                request = baseRequestResponse.getRequest()
+                requestInfo = self._extender._helpers.analyzeRequest(baseRequestResponse)
+                body = request[requestInfo.getBodyOffset():]
 
-        connStr = "Connection: Upgrade"
-        h2cRequestTwo = self._helpers.buildHttpMessage(newHeaders + [connStr],
-                                                       body)
+                headers = requestInfo.getHeaders()
+                newHeaders = []
+                for header in headers:
+                    if header.startswith("Connection") or header.startswith("Upgrade"):
+                        pass
+                    else:
+                        newHeaders.append(header)
+                newHeaders.append("Upgrade: h2c")
+                newHeaders.append("HTTP2-Settings: AAMAAABkAARAAAAAAAIAAAAA")
 
-        # Send Requests
-        requestResponseOne = self._callbacks.makeHttpRequest(
-                                baseRequestResponse.getHttpService(),
-                                h2cRequestOne)
-        requestResponseTwo = self._callbacks.makeHttpRequest(
-                                baseRequestResponse.getHttpService(),
-                                h2cRequestTwo)
+                connStr = "Connection: Upgrade, HTTP2-Settings"
+                h2cRequestOne = self._extender._helpers.buildHttpMessage(newHeaders + [connStr], body)
+                connStr = "Connection: Upgrade"
+                h2cRequestTwo = self._extender._helpers.buildHttpMessage(newHeaders + [connStr], body)
 
-        # Analyze responses
-        responseOneInfo = self._helpers.analyzeResponse(
-                                requestResponseOne.getResponse())
-        responseTwoInfo = self._helpers.analyzeResponse(
-                                requestResponseTwo.getResponse())
+                self._extender._callbacks.printOutput("Sending first h2c request...")
+                requestResponseOne = self._extender._callbacks.makeHttpRequest(
+                    baseRequestResponse.getHttpService(),
+                    h2cRequestOne)
+                self._extender._callbacks.printOutput("Sending second h2c request...")
+                requestResponseTwo = self._extender._callbacks.makeHttpRequest(
+                    baseRequestResponse.getHttpService(),
+                    h2cRequestTwo)
 
-        ret = []
-        if responseOneInfo.getStatusCode() == 101:
-            ret.append(CustomScanIssue(
-                baseRequestResponse.getHttpService(),
-                requestInfo.getUrl(),
-                [requestResponseOne],
-                "HTTP/2 Cleartext (h2c) Upgrade Support Detected",
-                """Server responded with 101 Switching Protocols. If this
-                upgrade response is from a backend server behind a proxy, then
-                intermediary proxy access controls (e.g., path and/or header
-                restrictions) can be bypassed by using
-                h2cSmuggler (https://github.com/BishopFox/h2csmuggler).""",
-                confidence))
+                self._extender._callbacks.addToSiteMap(requestResponseOne)
+                self._extender._callbacks.addToSiteMap(requestResponseTwo)
 
-        if responseTwoInfo.getStatusCode() == 101:
-            ret.append(CustomScanIssue(
-                baseRequestResponse.getHttpService(),
-                requestInfo.getUrl(),
-                [requestResponseTwo],
-                """"HTTP/2 Cleartext (h2c) Upgrade Support Detected",
-                Server responded with 101 Switching Protocols. If this
-                upgrade response is from a backend server behind a proxy, then
-                intermediary proxy access controls (e.g., path and/or header
-                restrictions) can be bypassed by using
-                h2cSmuggler (https://github.com/BishopFox/h2csmuggler).
-                <br><br> This instance did not require a Connection header to
-                forward the HTTP2-Settings header.
-                (use h2cSmuggler's --upgrade-only option)""",
-                confidence))
+                responseOneInfo = self._extender._helpers.analyzeResponse(requestResponseOne.getResponse())
+                responseTwoInfo = self._extender._helpers.analyzeResponse(requestResponseTwo.getResponse())
 
-        if len(ret) == 0:
-            return None
+                self._extender._callbacks.printOutput("Response 1 status: " + str(responseOneInfo.getStatusCode()))
+                self._extender._callbacks.printOutput("Response 2 status: " + str(responseTwoInfo.getStatusCode()))
 
-        return ret
+                if responseOneInfo.getStatusCode() == 101 or responseTwoInfo.getStatusCode() == 101:
+                    confidence = "Certain"
+                    if baseRequestResponse.getHttpService().getProtocol() != "https":
+                        confidence = "Tentative"
 
-    def consolidateDuplicateIssues(self, existingIssue, newIssue):
-        if existingIssue.getUrl() == newIssue.getUrl():
-            return -1
-
-        return 0
+                    issue = CustomScanIssue(
+                        baseRequestResponse.getHttpService(),
+                        requestInfo.getUrl(),
+                        [requestResponseOne, requestResponseTwo],
+                        "HTTP/2 Cleartext (h2c) Upgrade Support Detected",
+                        """Server responded with 101 Switching Protocols. If this
+                        upgrade response is from a backend server behind a proxy, then
+                        intermediary proxy access controls (e.g., path and/or header
+                        restrictions) can be bypassed by using
+                        h2cSmuggler (https://github.com/BishopFox/h2csmuggler).""",
+                        confidence)
+                    
+                    self._extender._callbacks.addScanIssue(issue)
+                    self._extender._callbacks.printOutput("Vulnerability detected and issue created!")
+            except Exception as e:
+                self._extender._callbacks.printError("Error processing request: " + str(e))
 
 
 class CustomScanIssue (IScanIssue):
-
-    def __init__(self, httpService, url, httpMessages, name, detail,
-                 confidence):
+    def __init__(self, httpService, url, httpMessages, name, detail, confidence):
         self._httpService = httpService
         self._url = url
         self._httpMessages = httpMessages
